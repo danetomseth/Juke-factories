6932d19b0e1c012b030c2a1646a087abdf0cabdb
danetomseth <danetomseth@gmail.com>
2016-03-29 18:26:00 -0400

    starting on All albums controller

---
+++ starting-point/browser/js/album/album.factory.js
@@ -0,0 +1,60 @@
+juke.factory('StatsFactory', function ($q) {
+  var statsObj = {};
+  statsObj.totalTime = function (album) {
+    var audio = document.createElement('audio');
+    return $q(function (resolve, reject) {
+      var sum = 0;
+      var n = 0;
+      function resolveOrRecur () {
+        if (n >= album.songs.length) resolve(sum);
+        else audio.src = album.songs[n++].audioUrl;
+      }
+      audio.addEventListener('loadedmetadata', function () {
+        sum += audio.duration;
+        resolveOrRecur();
+      });
+      resolveOrRecur();
+    });
+  };
+  return statsObj;
+});
+
+juke.factory('AlbumFactory', function($http, $log){
+  var allAlbums = [];
+
+  return {
+    fetchAll: function(){
+      return $http.get('/api/albums/')
+        .then(function(res) {
+            console.log('all data',res.data);
+            return res.data;
+        })
+        .catch($log.error); // $log service can be turned on and off; also, pre-bound
+    },
+    fetchOneAlbum: function(album){
+        return $http.get('/api/albums/' + album._id) 
+        .then(function(res) {
+            return res.data
+        })
+        .then(function(album) {
+            album.imageUrl = '/api/albums/' + album._id + '.image';
+            album.songs.forEach(function(song, i) {
+                song.audioUrl = '/api/songs/' + song._id + '.audio';
+                song.albumIndex = i;
+            })
+            console.log(album)
+            return album
+          })
+        .catch($log.error); // $log service can be turned on and off; also, pre-bound
+    },
+
+    fetchById: function(){
+      return $http.get('/api/:albumId/')
+      .then(function(res){
+        return res.data
+      })
+
+    }
+  }
+})
+
